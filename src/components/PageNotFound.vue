024c121e43867f100fbda0042fceeae3617e0de5
r1d3rzz <myintthwaykhine127@gmail.com>
2023-05-10 09:58:17 +0630

    create route middleware

---
+++ src/components/PageNotFound.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <div class="card">
+      <div class="card-body text-center">
+        <div class="fs-3">
+          <div class="text-danger">404</div>
+          <div>Page Not Found</div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {};
+</script>
+<style></style>
--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,8 @@ import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
 import CreateCredit from "../views/CreateCredit.vue";
 import ProfileView from "../views/users/ProfileView.vue";
+import PageNotFound from "../components/PageNotFound.vue";
+import { auth } from "@/firebase/config";
 
 const routes = [
   {
@@ -13,11 +15,32 @@ const routes = [
     path: "/users/credit/create",
     name: "createCredit",
     component: CreateCredit,
+    beforeEnter: (to, from, next) => {
+      let user = auth.currentUser;
+      if (user) {
+        next();
+      } else {
+        next({ name: "home" });
+      }
+    },
   },
   {
     path: "/users/profile",
     name: "userProfile",
     component: ProfileView,
+    beforeEnter: (to, from, next) => {
+      let user = auth.currentUser;
+      if (user) {
+        next();
+      } else {
+        next({ name: "home" });
+      }
+    },
+  },
+  {
+    path: "/:catchAll(.*)",
+    name: "pageNotFound",
+    component: PageNotFound,
   },
 ];
 
