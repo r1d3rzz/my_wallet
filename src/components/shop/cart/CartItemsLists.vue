d8b38c40432b91031b38ad3617d9217c015609ea
r1d3rzz <myintthwaykhine127@gmail.com>
2023-05-18 15:25:38 +0630

    show user wallet info and suitable action button in cartSummary component

--- src/components/shop/cart/CartItemsLists.vue
+++ src/components/shop/cart/CartItemsLists.vue
@@ -2,7 +2,9 @@
   <div class="col-lg-8">
     <div class="p-5">
       <div class="d-flex justify-content-between align-items-center mb-5">
-        <h1 class="fw-bold mb-0 text-black">Shopping Cart</h1>
+        <h1 class="fw-bold-100 mb-0 text-black">
+          Check your items in the cart
+        </h1>
         <h6 class="mb-0 text-muted">{{ products.length }} items</h6>
       </div>
       <hr class="my-4" />
--- src/components/shop/cart/CartSummaryPayment.vue
+++ src/components/shop/cart/CartSummaryPayment.vue
@@ -10,7 +10,50 @@
       </div>
 
       <div class="mb-4">
-        <button class="btn btn-dark w-100">Please Login First</button>
+        <div v-if="user">
+          <div v-if="!makeAWallet">
+            <div v-if="userCard.length">
+              <div class="card">
+                <div class="text-center fw-bold card-header">Wallet Info</div>
+
+                <div
+                  class="card-body p-2"
+                  v-for="card in userCard"
+                  :key="card.id"
+                >
+                  <div class="mt-2">
+                    <div class="d-flex justify-content-between mb-2">
+                      <div class="fw-bold">No.</div>
+                      <div>{{ card.card_number }}</div>
+                    </div>
+
+                    <div class="d-flex justify-content-between mb-2">
+                      <div class="fw-bold">Balance</div>
+                      <div>{{ card.card_amount }} MMK</div>
+                    </div>
+
+                    <div class="d-flex justify-content-between mb-2">
+                      <div class="fw-bold">Mail</div>
+                      <div>{{ card.card_owner.email }}</div>
+                    </div>
+
+                    <div class="d-flex justify-content-between mb-2">
+                      <div class="fw-bold">Holder</div>
+                      <div>{{ card.card_owner.name }}</div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div v-else>loading...</div>
+          </div>
+          <div v-else>
+            <button class="btn btn-dark w-100">Make A Wallet</button>
+          </div>
+        </div>
+        <div v-else>
+          <button class="btn btn-dark w-100">Please Login First</button>
+        </div>
       </div>
 
       <hr class="my-4" />
@@ -22,7 +65,7 @@
 
       <button
         type="button"
-        disabled
+        :disabled="isDisable"
         class="btn btn-dark w-100"
         data-mdb-ripple-color="dark"
       >
@@ -33,14 +76,45 @@
 </template>
 <script>
 import store from "@/store";
-import { computed } from "vue";
+import { computed, ref } from "vue";
+import getUser from "@/composables/getUser";
+import getCards from "@/composables/getCards";
 
 export default {
   props: ["products"],
   setup() {
     let totalPrices = computed(() => store.getters.cartTotalPrice);
+    let { user } = getUser();
+    let { cards, load } = getCards();
+    let cardEmails = ref([]);
+    let userCard = ref([]);
+    let isDisable = ref(true);
+    let makeAWallet = ref(false);
 
-    return { totalPrices };
+    let getAllCards = async () => {
+      await load();
+      cards.value.forEach((card) => {
+        cardEmails.value.push(card.card_owner.email);
+      });
+
+      if (cardEmails.value.includes(user.value.email)) {
+        let usersCards = cards.value.filter((card) => {
+          return card.card_owner.email == user.value.email;
+        });
+
+        userCard.value.push(usersCards[0]);
+      } else {
+        isDisable.value = true;
+        makeAWallet.value = true;
+      }
+    };
+
+    if (user.value) {
+      getAllCards();
+      isDisable.value = false;
+    }
+
+    return { totalPrices, user, cards, userCard, isDisable, makeAWallet };
   },
 };
 </script>
